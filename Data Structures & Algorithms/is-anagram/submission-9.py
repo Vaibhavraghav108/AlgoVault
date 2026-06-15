2be31e4aa9b8a672a01e35d894a57cce6dbb9fa5
Vaibhav <vaibhavraghav14@gmail.com>
2026-06-15 14:14:13 +0530

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        dict={}
+        for i in s:
+            if i in dict:
+                dict[i]+=1
+            else:
+                dict[i]=1
+        for i in t:
+            if i in dict:
+                dict[i]-=1
+            else:
+                return False
+        if not all(value==0 for value in dict.values()):
+            return False
+        return True
+        
+            
