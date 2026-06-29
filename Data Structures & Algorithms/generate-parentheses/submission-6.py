fc75f95c725986ab234a9e4dd7041cd06f8d8b4e
Vaibhav <vaibhavraghav14@gmail.com>
2026-06-29 22:03:33 +0530

    Add: generate-parentheses - submission-6

---
+++ Data Structures & Algorithms/generate-parentheses/submission-6.py
@@ -0,0 +1,42 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result=[]
+        sol=[]
+        total=n*2
+        '''
+        def backtrack(openp,closep):
+            if openp==n and closep==n:
+                result.append("".join(sol[:]))
+                return
+            if openp<n:
+                sol.append('(')
+                backtrack(openp+1,closep)
+                sol.pop()
+            if closep<openp:
+                sol.append(')')
+                backtrack(openp,closep+1)
+                sol.pop()
+        backtrack(0,0)
+        return result
+        '''
+        def backtrack(i,count):
+            if count<0:
+                return
+            if count>total//2:
+                return
+            if i==total:
+                if count==0:
+                    result.append("".join(sol[:]))
+                return
+            # take (
+            sol.append('(')
+            backtrack(i+1,count+1)
+            sol.pop()
+
+            # take )
+            if count>0:
+                sol.append(')')
+                backtrack(i+1,count-1)
+                sol.pop()
+        backtrack(0,0)
+        return result
